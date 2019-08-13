8ace3c5f55cca95a6ae3fd86a79947dbb1d0adc5
Tony Messias <tonyzrp@gmail.com>
2019-08-12 21:52:59 -0300

    Wrap the commands inside a docker container

--- app/Jobs/RunCommand.php
+++ app/Jobs/RunCommand.php
@@ -3,17 +3,22 @@
 namespace App\Jobs;
 
 use App\Command;
-use App\Events\CommandFinished;
 use Illuminate\Bus\Queueable;
+use App\Events\CommandFinished;
+use Illuminate\Encryption\Encrypter;
 use Illuminate\Queue\SerializesModels;
+use Symfony\Component\Process\Process;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessTimedOutException;
 
 class RunCommand implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    const PROC_TIMEOUT_IN_SECONDS = 4;
+    const TIMEOUT_EXIT_CODE = 124;
 
     /**
      * @var \App\Command
@@ -37,14 +42,51 @@ class RunCommand implements ShouldQueue
      */
     public function handle()
     {
-        $process = new Process($this->command->command, base_path());
-        $process->run();
+        $dockerWrapper = $this->wrapCommandInDocker();
 
-        $exitCode = $process->getExitCode();
-        $output = $process->getOutput();
+        $process = new Process(
+            $dockerWrapper . sprintf(' bash -c "%s"', $this->command->command),
+            base_path()
+        );
+        $process->setTimeout(static::PROC_TIMEOUT_IN_SECONDS);
+
+        try {
+            $process->run();
+            $exitCode = $process->getExitCode();
+            $output = $process->getOutput();
+        } catch (ProcessTimedOutException $exception) {
+            $exitCode = static::TIMEOUT_EXIT_CODE;
+            $output = $this->parseTimeoutMessage();
+        }
 
         $this->command->markAsFinished($exitCode, $output);
 
         event(new CommandFinished($this->command));
+    }
+
+    private function wrapCommandInDocker(): string
+    {
+        return sprintf(
+            'docker run -e "APP_KEY=%s" --rm --stop-timeout %d tonysm/laravel-app:1.0',
+            $this->generateKey(),
+            static::PROC_TIMEOUT_IN_SECONDS
+        );
+    }
+
+    private function generateKey(): string
+    {
+        return sprintf(
+            'base64:%s',
+            base64_encode(Encrypter::generateKey(config('app.cipher')))
+        );
+    }
+
+    private function parseTimeoutMessage(): string
+    {
+        return sprintf(
+            'Command "%s" exceeded a timeout of %s.',
+            $this->command->command,
+            trans_choice('{0} 0 seconds|{1} :count second|{2,*} :count seconds', static::PROC_TIMEOUT_IN_SECONDS)
+        );
     }
 }
