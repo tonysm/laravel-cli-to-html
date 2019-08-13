77cfb206fe505863e7de19294f6ced22be8607b8
Tony Messias <tonyzrp@gmail.com>
2019-08-12 21:52:38 -0300

    Add docblocks for autocompletion

--- app/Command.php
+++ app/Command.php
@@ -5,6 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use SensioLabs\AnsiConverter\AnsiToHtmlConverter;
 
+/**
+ * @property string $command
+ * @property int|null exit_code
+ * @property \Illuminate\Support\Carbon|null $completed_at
+ */
 class Command extends Model
 {
     protected $fillable = [
